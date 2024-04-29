35582c29d43eaa78873f2a8c044ddab2683157dc
klaintis <kcami32@outlook.com>
2024-04-29 22:58:27 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Components/Header.js';
+import Footer from './Components/Footer.js';
+
+function App() {
+  return (
+    <div className="App"> 
+       <Header />
+       <Footer />
+    </div>
+  );
+}
+
+export default App;
