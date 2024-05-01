4ba72a1da2f19d9b3484531bbf9960d72eae7132
klaintis <kcami32@outlook.com>
2024-05-01 15:53:33 +0300

    polles alages

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import './header.css';
 import DarkMode from './DarkMode/DarkMode.js';
 
 const Header = () => {
+  const location = useLocation();
   const [selectedLanguage, setSelectedLanguage] = useState(localStorage.getItem('selectedLanguage') || 'en');
+  const [showHeaderDiv, setShowHeaderDiv] = useState(true);
 
   const handleChange = (event) => {
     const selectedLang = event.target.value;
@@ -14,7 +16,9 @@ const Header = () => {
 
   useEffect(() => {
     localStorage.setItem('selectedLanguage', selectedLanguage);
-  }, [selectedLanguage]);
+    setShowHeaderDiv(location.pathname === '/');
+    // προσθεσε αυτο να θεσ να εμφανιστει και σε αλλα:  || location.pathname === '/contact');
+  }, [selectedLanguage, location]);
 
   return (
     <div className="header">
@@ -23,13 +27,15 @@ const Header = () => {
               <p><strong>Klajdi Cami</strong></p>
           </Link>
       </div>
-      <div className="right-item">
-          <select className="form-select" id="languages" name="languages" value={selectedLanguage} onChange={handleChange}>
-              <option value="en">English</option>
-              <option value="gr">Greek</option>
-              <option value="ge">German</option>
-          </select>
-      </div>
+      {showHeaderDiv && (
+        <div className="right-item">
+            <select className="form-select" id="languages" name="languages" value={selectedLanguage} onChange={handleChange}>
+                <option value="en">English</option>
+                <option value="gr">Greek</option>
+                <option value="ge">German</option>
+            </select>
+        </div>
+      )}
       <DarkMode />
     </div>
   );
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,31 @@
+import './projects.css';
+import Math from "../Assets/math.png";
 
 const Projects = () => {
+
     return (
-        <h1>Projects</h1>
+        <div class="container">
+            <br />
+            <div class="divmath">
+                <img src={Math} alt="" class="math" />
+            </div>
+            <div><h2>Projects</h2></div>
+            <br />
+            <form onsubmit="event.preventDefault();" role="search">
+                <input id="search" type="search" placeholder="Αναζήτηση..." autofocus required />
+                <button type="submit">Go</button>    
+            </form>
+            <br /><br />
+
+            <div id="dynamicContentContainer"></div>
+            
+            <div class="loadmore" id="loadmore">
+                <a>Load More</a><br />
+            </div>
+
+            <br /><br /><br /><br />
+
+        </div>
     );
 }
  
