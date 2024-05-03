3b0305f17dbade540371e7a9a1de0e0006339664
klaintis <kcami32@outlook.com>
2024-05-04 00:23:49 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-import {
-  createHashRouter,
-  RouterProvider
-} from 'react-router-dom';
-
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
-
