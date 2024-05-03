1f2459964c936574d39a6fc3687ad3bdd0919da3
klaintis <kcami32@outlook.com>
2024-05-03 21:52:57 +0300

    .

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -6,61 +6,97 @@ import xeraki from '../Assets/output-onlinegiftools.gif';
 
 const Books = () => {
     const [infoVisible, setInfoVisible] = useState({});
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredBooks, setFilteredBooks] = useState(books);
+    const [searched, setSearched] = useState(false);
+    const [toxeraki, setToxeraki] = useState(false);
 
     const toggleDivVisibility = (bookId) => {
+        setToxeraki(true);
         setInfoVisible(prevState => ({
         ...prevState,
         [bookId]: !prevState[bookId]
         }));
     };
 
+    const handleSearch = (event) => {
+        event.preventDefault();
+        setSearched(true);
+        const filtered = books.filter(book => {
+            const lowerSearchTerm = searchTerm.toLowerCase();
+            return (
+                book.Titlos.toLowerCase().includes(lowerSearchTerm) ||
+                book.Katigoria.toLowerCase().includes(lowerSearchTerm) ||
+                book.sxolio_simperasma.toLowerCase().includes(lowerSearchTerm) 
+            );
+        });
+        setFilteredBooks(filtered);
+    }
+    
+
     return (  
         <div className="container">
             <br/>
             <div className='center'><h2>Books 📚 i have read and liked 👍</h2></div>
             <br/>
-            <form role="search">
-                <input id="search" type="search" placeholder="Αναζήτηση..." autoFocus required />
+            <form role="search" onSubmit={handleSearch}>
+                <input 
+                    id="search" 
+                    type="search" 
+                    placeholder="Αναζήτηση..." 
+                    autoFocus 
+                    required 
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
                 <button type="submit">Go</button>    
             </form>
             <br/><br/>
 
-                <div id="tokrifo">
-                    <div className="todivtoybiblioyPIN">
-                        <div className="left-item2">
-                            <p>Make Your Bed: Little Things That Can Change Your Life...And Maybe the World, by William H. McRaven</p>
+                {searched ? null : (
+                    <div id="tokrifo">
+                        <div className="todivtoybiblioyPIN">
+                            <div className="left-item2">
+                                <p>Make Your Bed: Little Things That Can Change Your Life...And Maybe the World, by William H. McRaven</p>
+                            </div>
+
+                            <img src={Pin} alt="" />
                         </div>
 
-                        <img src={Pin} alt="" />
-                    </div>
-
-                    <div className="toinfoPIN">
+                        <div className="toinfoPIN">
+                            <br/>
+                            <p><strong>Category: </strong>Personal Growth & Self improvement</p>
+                            <p><strong>Comment / Conclusion: </strong>The basic idea of the book is that: If you want to change the world, start off by making your bed. If every day we wake up we make our bed, this will give us motivation to continue doing things during the day, this also reinforces the fact that small things in life have enormous significance.</p>
+                            <br/>
+                        </div>
                         <br/>
-                        <p><strong>Category: </strong>Personal Growth & Self improvement</p>
-                        <p><strong>Comment / Conclusion: </strong>The basic idea of the book is that: If you want to change the world, start off by making your bed. If every day we wake up we make our bed, this will give us motivation to continue doing things during the day, this also reinforces the fact that small things in life have enormous significance.</p>
                     </div>
-                    <br/>
-                </div>
+                )}
 
                 <div id="dynamicContentContainer">
-                    {books.map((book, index) => (
+                    {filteredBooks.length === 0 ? (
+                        <p className='notfound'>No books was found that contains the word - phrase: <strong>"{searchTerm}"</strong>...</p>
+                    ) : (
+                    filteredBooks.map((book, index) => (
                         <div key={index}>
                             <div className="todivtoybiblioy" onClick={() => toggleDivVisibility(index)}>
                                 <div className="left-item2">
                                     <p>{book.Titlos}</p>
                                 </div>
-                                {book.xeraki ? <img src={xeraki} alt="" id="xerakigif" /> : null}
+                                {toxeraki ? null : (book.xeraki ? <img src={xeraki} alt="" id="xerakigif" /> : null)}
                             </div>
                             {infoVisible[index] && (
                                 <div className="toinfo">
-                                <br/>
-                                <p><strong>Category: </strong>{book.Katigoria}</p>
-                                <p><strong>Comment / Conclusion: </strong>{book.sxolio_simperasma}</p>
+                                    <br/>
+                                    <p><strong>Category: </strong>{book.Katigoria}</p>
+                                    <p><strong>Comment / Conclusion: </strong>{book.sxolio_simperasma}</p>
+                                    <br/>
                                 </div>
                             )}
                             <br/> 
                         </div>      
-                    ))}
+                    ))
+                )}
                 </div>
 
                 <div className="waves" id="loadingDiv">
--- src/Components/Dap.js
+++ src/Components/Dap.js
@@ -1,7 +1,78 @@
+import './dap.css';
+import React, { useState, useEffect } from 'react';
+
 
 const Dap = () => {
+    const [apodData, setApodData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        fetch(`https://api.nasa.gov/planetary/apod?api_key=wNu2CW6MkRKeGOLgiAeLxoxVkLPGhQpj0KIqz0Tf`)
+            .then(response => response.json())
+            .then(data => {
+                setApodData(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching APOD:', error);
+                setError(true);
+                setLoading(false);
+            });
+    }, []); 
+
+    if (loading) {
+        return <div className='center'>Loading...</div>;
+    }
+
+    if (error) {
+        return <div className='center'>Error fetching APOD data</div>;
+    }
+
+    if (!apodData) {
+        return null;
+    }
+
     return (
-        <h1>Dap</h1>
+        <div className="container">
+
+            <div className="loading" id="loadingDiv">
+                <img src="images/marsloading.gif" alt="" />
+            </div>
+
+            <div className="toerror" id="errorDiv">
+                <img src="images/error.png" alt="" />
+            </div>
+
+            <div className="kinito" id="kinitoDiv">
+                <div className='center'><h2>🌌 NASA's Astronomy Picture<br/>/ Video of the Day</h2></div>
+                <br/>
+                <div className="image-container" id="imageContainer" >
+                    {apodData.media_type === 'image' ? (
+                        <img src={apodData.url} className="NasaImg" alt={apodData.title} />
+                    ) : apodData.media_type === 'video' ? (
+                        <iframe
+                            width="90%"
+                            height="600"
+                            src={apodData.url}
+                            title={apodData.title}
+                            frameBorder="1"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowFullScreen
+                        ></iframe>
+                    ) : null}
+                    <br/><br/>
+                </div>          
+                
+                <p><strong>Caption:</strong> <span id="apodCaption">{apodData.title}</span><br/><br/>
+                <strong>Date:</strong> <span id="apodDate">{apodData.date}</span><br/><br/>
+                <strong>Explanation:<br/><br/></strong> <span id="apodExplanation">{apodData.explanation}</span><br/><br/>
+                
+                <strong>References:</strong> <a href="https://apod.nasa.gov/apod/" className="toa1">Apod.nasa.gov</a><br/><br/>
+                <strong>Data provided by NASA Astronomy Picture of the Day (APOD)</strong> <a href="https://api.nasa.gov/planetary/apod" className="toa1">API</a></p>
+                <br/><br/><br/><br/><br/><br/>
+            </div>
+        </div>
     );
 }
  
