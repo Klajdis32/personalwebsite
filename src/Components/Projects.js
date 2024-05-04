ea9c31d9f7ae06d33233bd6bd5d98b4f7b3d74a5
klaintis <kcami32@outlook.com>
2024-05-04 18:30:47 +0300

    /

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,10 +2,22 @@ import React, { useState } from 'react';
 import './projects.css';
 import posts from './ProjectsDB.js'; 
 import Math from "../assets/math.png";
+import pdf from "../assets/pdf.png";
+import link from "../assets/link.png";
+import video from "../assets/video.png";
+import folder from "../assets/folder.png";
 
 const Projects = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [filteredPosts, setFilteredPosts] = useState(posts);
+    const [postStates, setPostStates] = useState({});
+
+    const toggleText = (index) => {
+        const newPostStates = { ...postStates };
+        newPostStates[index] = !newPostStates[index];
+        setPostStates(newPostStates);
+    };
+
 
     const handleSearch = (event) => {
         event.preventDefault();
@@ -63,22 +75,36 @@ const Projects = () => {
                                         <span>{post.Katigoria}</span>
                                     </div>
                                     <h1 className="blog-post_title">{post.Titlos}</h1>
-                                    <p className="blog-post_text">{post.Keimeno}</p>
+                                    <p className="blog-post_text">
+                                        {post.Keimeno.length > 300 ? (
+                                            <>
+                                                {post.Keimeno.substring(0, 300)}
+                                                <span id={`toggleText${index}`} style={{ display: postStates[index] ? 'inline' : 'none' }}>
+                                                    {post.Keimeno.substring(300)}
+                                                </span>
+                                                <a className='toshow' onClick={() => toggleText(index)}>
+                                                    {postStates[index] ? 'show less' : '...see more'}
+                                                </a>
+                                            </>
+                                        ) : (
+                                            post.Keimeno
+                                        )}
+                                    </p>
                                     <div className="todivmetaa">
                                         {post.Link && (
-                                            <a href={post.Link} target="_blank" className="blog-post_cta"><img src="images/link.png" alt="" className="todivmeimages" />Link</a>
+                                            <a href={post.Link} target="_blank" className="blog-post_cta"><img src={link} alt="" className="todivmeimages" />Link</a>
                                         )}
                                         {post.pdf && (
                                             <a href={post.pdf} download={post.pdf} className="blog-post_cta">
-                                                <img src="images/pdf.png" alt="" className="todivmeimages" />
+                                                <img src={pdf} alt="" className="todivmeimages" />
                                                 Pdf
                                             </a>
                                         )}
                                         {post.file && (
-                                            <a href={post.file} className="blog-post_cta"><img src="images/folder.png" alt="" className="todivmeimages" />File</a>
+                                            <a href={post.file} className="blog-post_cta"><img src={folder} alt="" className="todivmeimages" />File</a>
                                         )}
                                         {post.video && (
-                                            <a href={post.video} target="_blank" className="blog-post_cta"><img src="images/video.png" alt="" className="todivmeimages" />Video</a>
+                                            <a href={post.video} target="_blank" className="blog-post_cta"><img src={video} alt="" className="todivmeimages" />Video</a>
                                         )}
                                     </div>
                                 </div>
