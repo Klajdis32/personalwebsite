47473210559aa526b7138bed31f0b9f8861059e3
klaintis <kcami32@outlook.com>
2024-05-09 20:00:30 +0300

    Final

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Header from './Components/Header.js';
+import Footer from './Components/Footer.js';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom'; 
+import Home from './Components/Home.js'; 
+import Projects from './Components/Projects.js';
+import Contact from './Components/Contact.js'; 
+import Dap from './Components/Dap.js'; 
+
+function App() {
+  return (
+    <Router>
+        <Header /> 
+          <Routes>
+            <Route path="/" exact element={<HomeWithHeader />} /> 
+            <Route path="/projects" element={<ProjectsWithHeader />} /> 
+            <Route path="/contact" element={<ContactWithHeader />} /> 
+            <Route path="/dap" element={<DapWithHeader />} />
+          </Routes>
+        <Footer />
+    </Router>
+  );
+}
+
+function HomeWithHeader() {
+  useDocumentTitle("Carpe diem");
+  return <Home />;
+}
+
+function ProjectsWithHeader() {
+  useDocumentTitle("Projects");
+  return <Projects />;
+}
+
+function ContactWithHeader() {
+  useDocumentTitle("Contact");
+  return <Contact />;
+}
+
+function DapWithHeader() {
+  useDocumentTitle("Picture-Video of the day");
+  return <Dap />;
+}
+
+function useDocumentTitle(title) {
+  const location = useLocation();
+  useEffect(() => {
+    document.title = title;
+  }, [location, title]);
+}
+
+export default App;
