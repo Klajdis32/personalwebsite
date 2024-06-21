21d3bb28bb6a1d89b9d5de28b77fd2ae07bae1b2
klaintis <kcami32@outlook.com>
2024-06-21 18:05:49 +0300

    !

--- src/Components/Dap.js
+++ src/Components/Dap.js
@@ -70,8 +70,8 @@ const Dap = () => {
                     <br/>
                 </div>   
                 <br/>
-                <div className='todatedeksia'><span id="apodDate">{apodData.date}</span></div><br/>
-                <p><div className='kentrototitle'><span id="apodCaption">{apodData.title}</span></div><br/><br/>
+                <div className='todatedeksia'><span id="apodDate">{apodData.date}</span></div>
+                <p><div className='kentrototitle'><span id="apodCaption">{apodData.title}</span></div><br/>
                 <strong>Explanation:<br/><br/></strong> <span id="apodExplanation">{apodData.explanation}</span><br/><br/>
                 
                 <strong>References:</strong> <a href="https://apod.nasa.gov/apod/" className="toa1">Apod.nasa.gov</a><br/><br/>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,36 +1,51 @@
 import { Link, useLocation } from 'react-router-dom';
 import './footer.css';
-import profilePicture from '../assets/user12.png';
-import projectsPicture from '../assets/planning.png';
-import contactPicture from '../assets/send1.png';
-import dapPicture from '../assets/astronomy.png';
+import profilePicture from '../assets/userblack.png';
+import projectsPicture from '../assets/projectblack.png';
+import contactPicture from '../assets/sendblack.png';
+import dapPicture from '../assets/dapblack.png';
+
+import profilexroma from '../assets/user.png';
+import projectmexroma from '../assets/projects.png';
+import contactmexroma from '../assets/send1.png';
+import dapmexroma from '../assets/dap.png';
 
 const Footer = () => {
   const location = useLocation();
+  const defaultProfilePicture = profilePicture;
+  const alternateProfilePicture = profilexroma;
 
+  const defaultprojectPicture = projectsPicture;
+  const alternateprojectPicture = projectmexroma;
+
+  const defaultcontactPicture = contactPicture;
+  const alternatecontactPicture = contactmexroma;
+
+  const defaultdapPicture = dapPicture;
+  const alternatedapPicture = dapmexroma;
    return (
     <div className="footer">
       <Link to="/" className={`toa ${location.pathname === '/' ? 'active' : ''}`}>
         <div className="todiv">
-          <img src={profilePicture} alt="" /><br/>
+          <img src={location.pathname === '/' ? alternateProfilePicture : defaultProfilePicture} alt="Profile" /><br />
           <span>About me</span>
         </div>
       </Link>
       <Link to="/projects" className={`toa ${location.pathname === '/projects' ? 'active' : ''}`}>
         <div className="todiv">
-          <img src={projectsPicture} alt="" /><br/>
+        <img src={location.pathname === '/projects' ? alternateprojectPicture : defaultprojectPicture} alt="Profile" /><br />
           <span>Projects</span>
         </div>
       </Link>
       <Link to="/contact" className={`toa ${location.pathname === '/contact' ? 'active' : ''}`}>
         <div className="todiv">
-          <img src={contactPicture} alt="" /><br/>
+        <img src={location.pathname === '/contact' ? alternatecontactPicture : defaultcontactPicture} alt="Profile" /><br />
           <span>Contact</span>
         </div>
       </Link>
       <Link to="/dap" className={`toa ${location.pathname === '/dap' ? 'active' : ''}`}>
         <div className="todiv">
-          <img src={dapPicture} alt="" /><br/>
+        <img src={location.pathname === '/dap' ? alternatedapPicture : defaultdapPicture} alt="Profile" /><br />
           <span>DAP</span>
         </div>
       </Link>
